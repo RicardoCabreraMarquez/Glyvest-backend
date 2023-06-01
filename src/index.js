f59046f7a9e49c0c26444d8eb437df0d796ac682
RicardoCabreraMarquez <95092615+RicardoCabreraMarquez@users.noreply.github.com>
2023-06-01 12:45:24 +0200

    cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const loaders = require('./loaders');
 const config = require('./config');
 const { logger } = require('./utils');
 
 const app = express();
+
+app.use(cors());
 
 const { puerto } = config;
 
