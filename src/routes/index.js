ee7f9d26d12558af6315fa09d21d2dfc8f2e34f6
RicardoCabreraMarquez <95092615+RicardoCabreraMarquez@users.noreply.github.com>
2023-06-07 13:20:57 +0200

    comprobación usuario/contraseña BBDD

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { User } = require('../models');
 
 const { paginaInicio } = require('../controllers');
 const UserRouterUserMongodb = require('./user');
@@ -12,7 +13,29 @@ const router = express.Router();
 router.get('/inicio', paginaInicio);
 router.use('/user', UserRouterUserMongodb);
 router.use('/file', UserRouterPublicationMongodb);
-router.post('/login', authController.login);
+router.post('/login', authController.login, (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Buscar el usuario en la base de datos
+    const usuario = User.findOne({ username });
+
+    if (!usuario) {
+      return res.status(401).json({ error: 'Usuario y/o contraseña incorrectos' });
+    }
+
+    if (password !== usuario.password) {
+      return res.status(401).json({ error: 'Usuario y/o contraseña incorrectos' });
+    }
+
+    // Inicio de sesión exitoso
+    return res.json({ message: 'Inicio de sesión exitoso' });
+  } catch (error) {
+    console.error('Error al iniciar sesión:', error);
+    return res.status(500).json({ error: 'Error al iniciar sesión' });
+  }
+});
+
 router.post('/correo', verifyToken, smtpController.enviarCorreo);
 
 module.exports = router;
