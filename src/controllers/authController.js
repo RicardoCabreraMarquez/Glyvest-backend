b8d761c97fc6229868b187847dfb41be3b497909
RicardoCabreraMarquez <95092615+RicardoCabreraMarquez@users.noreply.github.com>
2023-06-08 10:50:52 +0200

    enviar datos usuarios

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,7 +17,7 @@ async function login(req, res, next) {
       { expiresIn: '1h' },
     );
 
-    res.status(200).json({ token });
+    res.status(200).json({ token, user: username });
   } catch (error) {
     next(error);
   }
